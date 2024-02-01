adf68a47fac3705909e637b4a628fe225cfe4855
gwynethtann <S10257317@connect.np.edu.sg>
2024-02-02 04:37:41 +0800

    working updated vouchers info and points once redeemed

---
+++ js/points.js
@@ -0,0 +1,25 @@
+function deductPoints(button) {
+    var pointsToDeduct = parseInt(button.getAttribute("data-points"));
+    var userPointsElement = document.getElementById("userPoints");
+    var userPoints = parseInt(userPointsElement.innerText);
+    if (userPoints >= pointsToDeduct) { // redemption points update
+        userPoints -= pointsToDeduct;
+        userPointsElement.innerText = userPoints;
+
+        // select other vouchers data
+        var allRewardsCols = document.querySelectorAll('.rewardsCol');
+        allRewardsCols.forEach(function (rewardsCol) {
+            var voucherImage = rewardsCol.querySelector('img');
+            var claimButton = rewardsCol.querySelector('.colBtn1'); 
+            var pointsRequired = parseInt(claimButton.getAttribute("data-points"));
+
+            // change other vouchers' image and button color if cannot redeem
+            if (userPoints < pointsRequired) {
+                voucherImage.src = "Pictures/5.png";
+                claimButton.style.backgroundColor = "grey"; 
+            }
+        });
+    } else {
+        console.log("Insufficient points to redeem");
+    }
+}
