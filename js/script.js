2691ce0438b6e21269e4ebe4e435ec2c0824ba2e
gwynethtann <S10257317@connect.np.edu.sg>
2024-02-03 04:29:50 +0800

    changed naming convention and also ensure post is visible only when all inputs are avail

--- js/script.js
+++ js/script.js
@@ -1,60 +1,61 @@
 const apiBaseUrl = "https://api.sketchfab.com/v3/models/";
 const apiKey = "4351796cf6b8414498e5db4f437be245"; 
 
+//list of models 
 const modelIds = [
   "cacfc7474a604ed4b0275b8a3293eff6", "266316c0b16e4bf8abf7f8274e33deec", "d80bb55a259642d897e31bc038e1c396"
 ];
-
 
 //get variables from the uploadQNA
 document.addEventListener("DOMContentLoaded", function submitForm() {
   const modelQn = localStorage.getItem("title");
   const caption = localStorage.getItem("caption");
-  const embededCode = localStorage.getItem("embededCode"); 
+  const embeddedCode = localStorage.getItem("embeddedCode"); 
 
   //code test 
   console.log(`Model Name: ${modelQn}`);
   console.log(`Description: ${caption}`);
-  console.log(`Code: ${embededCode}`);
+  console.log(`Code: ${embeddedCode}`);
 
+  //to ensure formating of post available when inputs from uploadQNA is avail
+  if (modelQn && caption && embeddedCode){
   document.body.innerHTML += `
       <div class="columnOne">
-      <!--Post One-->
-      <div class="postColumnOne">
-        <div class="postProfile">
-          <div class="profilePicPadding">
-          <img src="https://media.sketchfab.com/avatars/78fa317e46024a5283765aa34df5e508/17fa177ffaa344a2b2fc8e78efd40fbb.jpeg">
-          </div> 
-          <h1>${modelQn}</h1>
-        </div>
-  
-        <div class="postImage">
-          <div class="sketchfab-embed-wrapper">
-          <div id="uploadNew"> </div>
+        <!--Post One-->
+        <div class="postColumnOne">
+          <div class="postProfile">
+            <div class="profilePicPadding">
+            <img src="https://media.sketchfab.com/avatars/78fa317e46024a5283765aa34df5e508/17fa177ffaa344a2b2fc8e78efd40fbb.jpeg">
+            </div> 
+            <h1>${modelQn}</h1>
+          </div>
+    
+          <div class="postImage">
+            <div class="sketchfab-embed-wrapper">
+            <div id="uploadNew"> </div>
+            </div>
+          </div>
+    
+          <div class="postIcons">
+            <img src="Pictures/Home - Heart Icon.png" />
+            <a href="comment.html">
+              <img src="Pictures/Home - Comment Icon.png" />
+            </a>
+          </div>
+    
+          <div class="postComment">
+            <h1>
+            ${caption}
+            </h1>
           </div>
         </div>
-  
-        <div class="postIcons">
-          <img src="Pictures/Home - Heart Icon.png" />
-          <a href="comment.html">
-            <img src="Pictures/Home - Comment Icon.png" />
-          </a>
-        </div>
-  
-        <div class="postComment">
-          <h1>
-          ${caption}
-          </h1>
-        </div>
-      </div>
       </div>
     </div>
       `;
     
     //add in embeded code from uploadQNA to QNA
-    document.getElementById("uploadNew").innerHTML =embededCode;
-
-
+    document.getElementById("uploadNew").innerHTML =embeddedCode;
+    ;}
 });
 
 function fetchModelDetails(modelId) {
@@ -83,37 +84,36 @@ function fetchModelDetails(modelId) {
       console.log(`Description: ${caption}`);
       console.log(`embedModel: ${embedModel}`);
       
-      
       document.body.innerHTML += `
       <div class="columnOne">
-      <!--Post One-->
-      <div class="postColumnOne">
-        <div class="postProfile">
-          <div class="profilePicPadding">
-          <img src="${profilePicture}">
-          </div> 
-          <h1>${modelQn}</h1>
-        </div>
-  
-        <div class="postImage">
-          <div class="sketchfab-embed-wrapper">
-          <iframe src="${embedModel}" allow="autoplay; fullscreen; vr" frameborder="0"></iframe>
+        <!--Post One-->
+        <div class="postColumnOne">
+          <div class="postProfile">
+            <div class="profilePicPadding">
+            <img src="${profilePicture}">
+            </div> 
+            <h1>${modelQn}</h1>
+          </div>
+    
+          <div class="postImage">
+            <div class="sketchfab-embed-wrapper">
+            <iframe src="${embedModel}" allow="autoplay; fullscreen; vr" frameborder="0"></iframe>
+            </div>
+          </div>
+    
+          <div class="postIcons">
+            <img src="Pictures/Home - Heart Icon.png" />
+            <a href="comment.html">
+              <img src="Pictures/Home - Comment Icon.png" />
+            </a>
+          </div>
+    
+          <div class="postComment">
+            <h1>
+            ${caption}
+            </h1>
           </div>
         </div>
-  
-        <div class="postIcons">
-          <img src="Pictures/Home - Heart Icon.png" />
-          <a href="comment.html">
-            <img src="Pictures/Home - Comment Icon.png" />
-          </a>
-        </div>
-  
-        <div class="postComment">
-          <h1>
-          ${caption}
-          </h1>
-        </div>
-      </div>
       </div>
     </div>
       `;
