6359c92b633e5b12bad8b14c32390bd79cac3f71
gwynethtann <S10257317@connect.np.edu.sg>
2024-02-07 15:12:14 +0800

    deleted spaces

--- js/challengepoints.js
+++ js/challengepoints.js
@@ -4,16 +4,17 @@ function joinChallenge (competitionPoint){
     console.log(`${userPoints}`);
     userPoints+= parseInt(competitionPoint);
     var borderColor =localStorage.getItem("borderColor");
+
+    //adding additional points according to what border color they have
     if (borderColor=="#7A601D"){userPoints+=1;}
     else if(borderColor=="#B7B7B7") {userPoints+=2;}
     else if (borderColor=="#D5B255"){userPoints+=3;}
+
     //update local storage for points
     localStorage.setItem("userPoints", userPoints);
     console.log(` ${userPoints}`);
+
     //redirect to sent page
     window.location.href = "sent.html";
 }
 
-//redirct to sent page 
-//function challengeDirect ()
-//{ window.location.href = "sent.html";}
